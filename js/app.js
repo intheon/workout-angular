9f13b8e5935b20a8ecd8680252ca7338c83b522e
intheon <allobon@gmail.com>
2015-01-07 12:21:51 +0000

    ahhhh more frameworks

--- js/app.js
+++ js/app.js
@@ -1,16 +1,55 @@
-angular.module("workout", []).controller(
-	"root", ["$scope", function($scope)
-	{
-		$scope.exercises = 
-		[
-		  {name: "Cross Trainer", calories: "16.6"},
-		  {name: "Running", calories: "11"},
-		  {name: "Rowing", calories: "10"},
-		  {name: "Stepper", calories: "7"}
-		];
+angular.module("workout", ["ui.router"])
 
-		$scope.addExercise = function()
-		{
-			$scope.exercises.push({name: "Exercise Name", calories: 0});
+  .config([
+  	"$stateProvider",
+  	"$urlRouterProvider",
+  	function($stateProvider, $urlRouterProvider)
+  	{
+  	  $stateProvider
+  	    .state("home", {
+  	    	url: "/home",
+  	    	templateUrl: "/home.html",
+  	    	controller:"root"
+  	    });
+
+  	  $urlRouterProvider.otherwise("home");
+  	}]);
+
+  .factory("posts", [function(){
+
+    var o = {
+  	  posts: []
+  	};
+
+  	return o;
+
+  }])
+
+  .controller(
+	"root", ["$scope", 'posts', function($scope, posts)
+	{
+	  $scope.exercises = 
+	  [
+	    {name: "Cross Trainer", calories: "16.6"},
+	    {name: "Running", calories: "11"},
+	    {name: "Rowing", calories: "10"},
+	    {name: "Stepper", calories: "7"}
+	  ];
+
+	  $scope.posts = posts.posts;
+
+	  $scope.addExercise = function()
+	    {
+	      if ( !$scope.calories || $scope.calories == ""){return}
+		  else if (!$scope.exercise || $scope.exercise == ""){return}
+		  else
+		  {
+		    $scope.exercises.push({
+		      name: $scope.exercise,
+		      calories: $scope.calories
+		    });
+		    $scope.exercise = "";
+		    $scope.calories = "";
+		  }
 		}
 	}]);
