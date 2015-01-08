83cb370c351f9e19d0e31aeb496bb7b7dd7621af
intheon <allobon@gmail.com>
2015-01-08 00:13:54 +0000

    probably broken

--- js/app.js
+++ js/app.js
@@ -1,55 +1,67 @@
-angular.module("workout", ["ui.router"])
+angular.module("workout", [])
 
-  .config([
-  	"$stateProvider",
-  	"$urlRouterProvider",
-  	function($stateProvider, $urlRouterProvider)
-  	{
-  	  $stateProvider
-  	    .state("home", {
-  	    	url: "/home",
-  	    	templateUrl: "/home.html",
-  	    	controller:"root"
-  	    });
-
-  	  $urlRouterProvider.otherwise("home");
-  	}]);
-
-  .factory("posts", [function(){
-
-    var o = {
-  	  posts: []
-  	};
-
-  	return o;
-
+.factory("exercises", [function()
+{
+	var o = {
+		exercises: [
+			{
+				name: "Cross Trainer",
+				calories: "16.6",
+				upvotes: 0
+			},
+			{
+				name: "Running",
+				calories: "11",
+				upvotes: 0
+			},
+			{
+				name: "Rowing",
+				calories: "10",
+				upvotes: 0
+			},
+			{
+				name: "Stepper",
+				calories: "7",
+				upvotes: 0
+			}
+	    ]
+	};
+	return o;
   }])
-
-  .controller(
-	"root", ["$scope", 'posts', function($scope, posts)
+.controller("root", [
+  	"$scope",
+  	"exercises",
+  	function($scope, exercises)
 	{
-	  $scope.exercises = 
-	  [
-	    {name: "Cross Trainer", calories: "16.6"},
-	    {name: "Running", calories: "11"},
-	    {name: "Rowing", calories: "10"},
-	    {name: "Stepper", calories: "7"}
-	  ];
-
-	  $scope.posts = posts.posts;
-
-	  $scope.addExercise = function()
-	    {
-	      if ( !$scope.calories || $scope.calories == ""){return}
-		  else if (!$scope.exercise || $scope.exercise == ""){return}
-		  else
-		  {
-		    $scope.exercises.push({
-		      name: $scope.exercise,
-		      calories: $scope.calories
-		    });
-		    $scope.exercise = "";
-		    $scope.calories = "";
-		  }
+		$scope.exercises = exercises.exercises;
+		$scope.addExercise = function()
+		{
+			if (!$scope.calories || $scope.calories == "")
+			{
+				return
+			}
+			else if (!$scope.exercise || $scope.exercise == "")
+			{
+				return
+			}
+			else
+			{
+				$scope.exercises.push(
+				{
+					name: $scope.exercise,
+					calories: $scope.calories,
+					upvotes: 0,
+  					comments: [
+    					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  					]
+				});
+				$scope.exercise = "";
+				$scope.calories = "";
+			}
 		}
-	}]);
+		$scope.incrementUpVotes = function(item)
+		{
+			item.upvotes += 1;
+		}
+	}])
